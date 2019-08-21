87760055583a9807276deecca7efa7f58ced9d2c
Levytskyi Bohdan <levytskyibohdan@gmail.com>
2019-08-21 14:26:08 +0300

    finished contact validdation

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -22,14 +22,19 @@ export class ContactsComponent implements OnInit {
   classPhone: boolean = false;
   classMessage: boolean = false;
 
+  classNameOk: boolean = false;
+  classEmailOk: boolean = false;
+  classPhoneOk: boolean = false;
+  classMessageOk: boolean = false;
+
   constructor(private ContactService: ContactService, private RegexService: RegexService, private ToastrService: ToastrService) { }
 
   ngOnInit() {
     this.getContact();
-    
+
   }
 
-  private getContact(): void{
+  private getContact(): void {
     this.ContactService.getContact().subscribe(actionArray => {
       this.contact = actionArray.map(item => {
         return {
@@ -40,34 +45,70 @@ export class ContactsComponent implements OnInit {
     });
   }
 
+  validateName(): void {
+    if (this.RegexService.validateName(this.name)) {
+      this.classNameOk = true;
+      this.className = false;
+    } else {
+      this.classNameOk = false;
+      this.className = true;
+    }
+  }
+  validateEmail(): void {
+    if (this.RegexService.validateEmail(this.email)) {
+      this.classEmailOk = true;
+      this.classEmail = false;
+    } else {
+      this.classEmailOk = false;
+      this.classEmail = true;
+    }
+  }
+  validatePhone(): void {
+    if (this.RegexService.validatePhone(this.phone)) {
+      this.classPhoneOk = true;
+      this.classPhone = false;
+    } else {
+      this.classPhoneOk = false;
+      this.classPhone = true;
+    }
+  }
+  validateMessage(): void {
+    if (this.message != undefined && this.message.length > 30) {
+      this.classMessageOk = true;
+      this.classMessage = false;
+    } else {
+      this.classMessageOk = false;
+      this.classMessage = true;
+    }
+  }
   sendMessage(form): void {
     // console.log(form.value);
     let checkName: boolean = false;
     let checkEmail: boolean = false;
     let checkPhone: boolean = false;
     let checkMessage: boolean = false;
-    if (this.RegexService.validateName(form.value.name) && form.value.name != undefined){
+    if (this.RegexService.validateName(form.value.name) && form.value.name != undefined) {
       checkName = true;
       this.className = false;
     } else {
       checkName = false;
       this.className = true;
     }
-    if (form.value.email != undefined && this.RegexService.validateEmail(form.value.email)){
+    if (form.value.email != undefined && this.RegexService.validateEmail(form.value.email)) {
       checkEmail = true;
       this.classEmail = false;
     } else {
-      checkEmail = false;      
+      checkEmail = false;
       this.classEmail = true;
     }
-    if (this.RegexService.validatePhone(form.value.phone) && form.value.phone != undefined){
+    if (this.RegexService.validatePhone(form.value.phone) && form.value.phone != undefined) {
       checkPhone = true;
       this.classPhone = false;
     } else {
-      checkPhone = false;      
+      checkPhone = false;
       this.classPhone = true;
     }
-    if (form.value.message != undefined){
+    if (form.value.message != undefined && form.value.message.length > 30) {
       checkMessage = true;
       this.classMessage = false;
     } else {
@@ -76,31 +117,40 @@ export class ContactsComponent implements OnInit {
     }
 
     if (checkName && checkEmail && checkPhone && checkMessage) {
-        // @ts-ignore
-        jivo_api.sendMessage({
-          "name": form.value.name,
-          "email": form.value.email,
-          "phone": form.value.phone,
-          "description": "Контакти. Повідомлення",
-          "message": form.value.message,
-        });
-        // @ts-ignore
-        jivo_api._sendIntroduce({
-          "name": form.value.name,
-          "email": form.value.email,
-          "phone": form.value.phone,
-        })
-        form.reset();
-        this.ToastrService.success('Наш представник невдовзі зв\'яжеться з вами', 'Повідомлення надіслано', {
-          timeOut: 6000,
-        })
+      // @ts-ignore
+      jivo_api.sendMessage({
+        "name": form.value.name,
+        "email": form.value.email,
+        "phone": form.value.phone,
+        "description": "Контакти. Повідомлення",
+        "message": form.value.message,
+      });
+      // @ts-ignore
+      jivo_api._sendIntroduce({
+        "name": form.value.name,
+        "email": form.value.email,
+        "phone": form.value.phone,
+      })
+      form.reset();
+      this.ToastrService.success('Наш представник невдовзі зв\'яжеться з вами', 'Повідомлення надіслано', {
+        timeOut: 6000,
+      })
+      this.className = false;
+      this.classEmail = false;
+      this.classPhone = false;
+      this.classMessage = false;
+
+      this.classNameOk = false;
+      this.classEmailOk = false;
+      this.classPhoneOk = false;
+      this.classMessageOk = false;
     } else {
       this.ToastrService.error('Заповніть форму', 'Помилка', {
         timeOut: 3000,
       });
     }
 
-    
+
   }
 
 }
